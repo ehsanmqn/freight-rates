1d49000fe43bf9e7e1fc2aa8593392cc91a06e0f
Ehsan <spin313@gmail.com>
2023-03-20 19:19:35 +0330

    Ports model added

--- ratestask_port/models.py
+++ ratestask_port/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from app import settings
+
+from ratestask_region.models import Regions
+
+
+class Ports(models.Model):
+    code = models.TextField(primary_key=True, default='', unique=True, blank=False, null=False,
+                            max_length=settings.PORT_CODE_MAX_LENGTH)
+    name = models.TextField(default='', blank=False, null=False, max_length=settings.PORT_NAME_MAX_LENGTH)
+    parent_slug = models.ForeignKey(Regions, on_delete=models.CASCADE, null=True, blank=True,
+                                    related_name='port_region')
+
+    class Meta:
+        verbose_name = 'port'
+        verbose_name_plural = 'ports'
+
+    def __str__(self):
+        return f'{self.code}'
