72423a50c5beaa8a7147ff961b8279e7bfe75da5
Ehsan <spin313@gmail.com>
2023-03-20 19:12:38 +0330

    Regions model description added

--- ratestask_region/models.py
+++ ratestask_region/models.py
@@ -4,6 +4,12 @@ from app import settings
 
 
 class Regions(models.Model):
+    """
+    Regions model represents the Region entity
+    This class is defined based on what specified about the region inside the task descriptions.
+    Overall, it does not have application in this task because the table that is created by django based on this entity
+    has different name in comparison to the initial database from the task.
+    """
     slug = models.TextField(primary_key=True, default='', unique=True, blank=False, null=False,
                             max_length=settings.REGION_SLUG_MAX_LENGTH)
     name = models.TextField(default='', blank=False, null=False, max_length=settings.REGION_NAME_MAX_LENGTH)
