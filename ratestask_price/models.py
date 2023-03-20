f63f51e16a4ec8b8ba080c12934e35ef6f15a2f1
Ehsan <spin313@gmail.com>
2023-03-20 19:28:55 +0330

    Prices model added

--- ratestask_price/models.py
+++ ratestask_price/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from ratestask_port.models import Ports
+
+
+class Prices(models.Model):
+    orig_code = models.ForeignKey(Ports, on_delete=models.CASCADE, null=True, blank=True, related_name='ports_origin')
+    dest_code = models.ForeignKey(Ports, on_delete=models.CASCADE, null=True, blank=True, related_name='ports_destin')
+    day = models.DateField()
+    price = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'price'
+        verbose_name_plural = 'prices'
+
+    def __str__(self):
+        return f'{self.price}'
