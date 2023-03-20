08087afecfe3bef08ac45164fe5869cfd90586c3
Ehsan <spin313@gmail.com>
2023-03-20 19:30:44 +0330

    on_delete updated

--- ratestask_port/models.py
+++ ratestask_port/models.py
@@ -15,7 +15,7 @@ class Ports(models.Model):
     code = models.TextField(primary_key=True, default='', unique=True, blank=False, null=False,
                             max_length=settings.PORT_CODE_MAX_LENGTH)
     name = models.TextField(default='', blank=False, null=False, max_length=settings.PORT_NAME_MAX_LENGTH)
-    parent_slug = models.ForeignKey(Regions, on_delete=models.CASCADE, null=True, blank=True,
+    parent_slug = models.ForeignKey(Regions, on_delete=models.SET_NULL, null=True, blank=True,
                                     related_name='port_region')
 
     class Meta:
