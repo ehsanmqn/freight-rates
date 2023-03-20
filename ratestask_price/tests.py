4532880dfd3413c878d8f3a77ad01ef954887cab
Ehsan <spin313@gmail.com>
2023-03-20 19:35:40 +0330

    Template test clas for the Prices model added

--- ratestask_price/tests.py
+++ ratestask_price/tests.py
@@ -1,3 +1,23 @@
+from datetime import date
+
 from django.test import TestCase
 
-# Create your tests here.
+from ratestask_port.models import Ports
+from ratestask_price.models import Prices
+
+
+class PricesModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Create two ports
+        Ports.objects.create(port_code='NYC00', port_name='New York City')
+        Ports.objects.create(port_code='LAX00', port_name='Los Angeles')
+
+        # Create a price object
+        Prices.objects.create(
+            orig_code=Ports.objects.get(port_code='NYC00'),
+            dest_code=Ports.objects.get(port_code='LAX00'),
+            day=date.today(),
+            price=1000,
+        )
