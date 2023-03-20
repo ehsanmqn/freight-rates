299ccf5962a97d0d262f18b51e0a6c63152f5acb
Ehsan <spin313@gmail.com>
2023-03-20 22:01:58 +0330

    setUpTestData updated according to the model to create data properly

--- ratestask_price/tests.py
+++ ratestask_price/tests.py
@@ -11,13 +11,13 @@ class PricesModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         # Create two ports
-        Ports.objects.create(port_code='NYC00', port_name='New York City')
-        Ports.objects.create(port_code='LAX00', port_name='Los Angeles')
+        Ports.objects.create(code='NYC00', name='New York City')
+        Ports.objects.create(code='LAX00', name='Los Angeles')
 
         # Create a price object
         Prices.objects.create(
-            orig_code=Ports.objects.get(port_code='NYC00'),
-            dest_code=Ports.objects.get(port_code='LAX00'),
+            orig_code=Ports.objects.get(code='NYC00'),
+            dest_code=Ports.objects.get(code='LAX00'),
             day=date.today(),
             price=1000,
         )
