47ea5d5689670e01887d6fc2fef8346d00a7186c
Ehsan <spin313@gmail.com>
2023-03-20 20:04:19 +0330

    Validation for checking whether origin code and destination code added to ListDailyAveragePriceInputSerializer

--- ratestask_price/serializers.py
+++ ratestask_price/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
 
+from ratestask_port.models import Ports
+
 
 class ListDailyAveragePriceInputSerializer(serializers.Serializer):
     date_from = serializers.DateField(required=True, allow_null=False)
     date_to = serializers.DateField(required=True, allow_null=False)
     origin = serializers.CharField(required=True, allow_blank=False, allow_null=False)
     destination = serializers.CharField(required=True, allow_blank=False, allow_null=False)
+
+    def validate_origin(self, value):
+        origin = Ports.get_ports_by_code_or_parent_slug(code=value, slug=value)
+
+        if len(origin) == 0:
+            raise serializers.ValidationError("Invalid origin port symbol.")
+        return origin
+
+    def validate_destination(self, value):
+        destination = Ports.get_ports_by_code_or_parent_slug(code=value, slug=value)
+
+        if len(destination) == 0:
+            raise serializers.ValidationError("Invalid destination port symbol.")
+        return destination
