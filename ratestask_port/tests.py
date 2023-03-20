a47303f2cdcb5699f9527fb76fc6d7dec2ec88ba
Ehsan <spin313@gmail.com>
2023-03-20 19:22:45 +0330

    Template TestCase class for the Ports model added

--- ratestask_port/tests.py
+++ ratestask_port/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+from ratestask_port.models import Ports
+from ratestask_region.models import Regions
+
+
+class PortsModelTestCase(TestCase):
+    """
+    Test class for the Ports model
+    """
+
+    def setUp(self):
+        self.region = Regions.objects.create(
+            slug='test_region',
+            name='Test Region'
+        )
+
+        self.port = Ports.objects.create(
+            code='TCODE',
+            name='Test Port',
+            parent_slug=self.region
+        )
