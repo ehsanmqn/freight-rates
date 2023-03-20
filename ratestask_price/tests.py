7cd514dc68a3c59f6ceb62240d1c0f2a1e16c5a4
Ehsan <spin313@gmail.com>
2023-03-20 19:38:33 +0330

    Other tests related to the fields label added

--- ratestask_price/tests.py
+++ ratestask_price/tests.py
@@ -24,8 +24,32 @@ class PricesModelTest(TestCase):
 
     def test_orig_code_label(self):
         """
-        Test verbose name of the orig_code
+        Test verbose name of the orig_code label
         """
         price = Prices.objects.get(id=1)
         field_label = price._meta.get_field('orig_code').verbose_name
         self.assertEqual(field_label, 'orig code')
+
+    def test_dest_code_label(self):
+        """
+        Test verbose name of the dest_code label
+        """
+        price = Prices.objects.get(id=1)
+        field_label = price._meta.get_field('dest_code').verbose_name
+        self.assertEqual(field_label, 'dest code')
+
+    def test_day_label(self):
+        """
+        Test verbose name of the day label
+        """
+        price = Prices.objects.get(id=1)
+        field_label = price._meta.get_field('day').verbose_name
+        self.assertEqual(field_label, 'day')
+
+    def test_price_label(self):
+        """
+        Test verbose name of the price label
+        """
+        price = Prices.objects.get(id=1)
+        field_label = price._meta.get_field('price').verbose_name
+        self.assertEqual(field_label, 'price')
