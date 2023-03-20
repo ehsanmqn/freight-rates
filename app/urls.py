854cb206ba73966df7ad518c7201d3d1b095339d
Ehsan <spin313@gmail.com>
2023-03-20 19:46:44 +0330

    list-daily-average-price route added

--- app/urls.py
+++ app/urls.py
@@ -4,12 +4,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from ratestask_price.views import ListDailyAveragePrice
 
 #
 # Version 1 APIs
 api_patterns_v1 = [
+    path("rates/", ListDailyAveragePrice.as_view(), name="list-daily-average-price")
 ]
-
 
 api_patterns = [
     path("v1/", include(api_patterns_v1), name="api-v1"),
